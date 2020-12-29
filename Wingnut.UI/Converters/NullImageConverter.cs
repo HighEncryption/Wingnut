034e9bc013833902924aa88f36a27543e6d5e257
HighEncryption <29420483+HighEncryption@users.noreply.github.com>
2020-12-28 23:12:00 -0800

    Fix null reference binding for HeaderImage XAML cleanup

---
+++ Wingnut.UI/Converters/NullImageConverter.cs
@@ -0,0 +1,25 @@
+﻿namespace Wingnut.UI.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    public class NullImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return DependencyProperty.UnsetValue;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
